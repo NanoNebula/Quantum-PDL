0adc89135f6dd93253af93ff01798d18d91a71b8
Mark Baker <NanoNebula@users.noreply.github.com>
2013-12-17 00:53:07 -0800

    Create electron.pl

---
+++ electron.pl
@@ -0,0 +1,33 @@
+
+
+use PDL;use PDL::Graphics::TriD;
+for$c(1..99) {
+ $n=6.28*$c;$v=$c*rvals(zeros(90000)*$c);
+  $z=-1**$v*$c;
+   $y=-1**$v*sin$v*$c;
+    $x=-1**$c*rvals($v)*$c;
+
+$w=$z-$y-$x;
+$g=sin($w);
+$r=cos($y+$c+$z);
+$b=cos($w);
+$q=$w*$n;
+
+
+$xx = $b*sin($q);
+$yy = $r*cos($q);
+$zz = $g*sin($q);
+
+$xxx = $xx*$yy;
+$yyy = $zz*$xx;
+$zzz = $yy*$zz;
+                      #x  y  z
+points3d[$xx,$yy,$zz],[$g,$b,$r]; ## electron 
+
+
+ points3d[$xxx,$yyy,$zzz],[$g*$y,$r*$g,$b*$r]; ## Hbar geometry
+
+
+
+
+}
