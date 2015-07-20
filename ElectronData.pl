3825744e9c8b5c2088bfb302388efd85fd2eed67
Mark Baker <NanoNebula@users.noreply.github.com>
2015-07-20 19:45:06 -0400

    Update ElectronData.pl

--- ElectronData.pl
+++ ElectronData.pl
@@ -1,18 +1,37 @@
 
 
- 
-use PDL;use PDL::Graphics::TriD;
-for$c(1..99) {
- $n=6.28*$c;$v=$c*rvals(zeros(90000)*$c);
-  $cz=-1**$v*$c;
-   $cy=-1**$v*sin$v*$c;
-    $cx=-1**$c*rvals($v)*$c;
+use PDL;
+use PDL::Graphics::TriD;
+use PDL::Math; keeptwiddling3d;
+### QPD  Quantum Photonic Dynamics
+### QPD::Math uses 3 transcendental [angle] equations
+###           To describe the geometry of the electron 
+###           not 2 [angles] and 1 [length] like the Spherical Coordinates system
+###           not 3 [lengths] like the  Cartesian coordinate system
+###           3 Transendental [angles] in a 3 [length]  Cartesian coordinate system
+###           that describes a dynamic vector geometry of a photon(s) (String(s)) 
+###           that make up a particle 
 
-$w=$cz-$cy-$cx;
-$g=sin($w);
-$r=cos($cy+$c+$cz);
-$b=cos($w);
-$q=$w*$n;
+
+ for $c(400..1999){
+ for $frequency (1..20) {
+ for $phase(1..360) {
+ $phase = $phase*0.0000001;
+ $n=6.28*$c*$frequency+($phase);
+
+ $photons = 2444; ### [Density]
+
+ $t=(-1**.5)*rvals(exp(zeros($photons)),{Centre=>[0]});
+ $cz=-1**$t*$c;
+ $cy=-1**$t*sin($t*$c);
+ $cx=-1**$c*bessj0(rvals($t,{Centre=>[0]}))*$c;
+ 
+ $w=$cz-$cy-$cx
+ $g=sin($w);
+ $r=cos($cy+$c+$cz);
+ $b=cos($w);
+ $i=($cz-$cx-$cy);
+  $q=$c*($i*$n);
 
 
 $x = $b*sin($q);
