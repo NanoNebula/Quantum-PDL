0595e7e3598ce37f3e77446f6e1abf47a5565fc1
NanoNebula <mrbaker_pdl@yahoo.com>
2017-02-05 00:38:31 -0800

    Update Electron.pl

--- Electron.pl
+++ Electron.pl
@@ -102,6 +102,9 @@ $z = $g*sin($q);  ### cos
   
   ### ((volts * farads) / (volts / omhs )) =  ([T]*[I]) - ([I]) =  [T] = 1 / time =  [T**-1] = frequency
   
+  ### speed of light / frequency = [L] = ($radius+$radius) = Length  
+  ###  [L] * [T**-1] / [T**-1] = Length
+  
   
   
    
